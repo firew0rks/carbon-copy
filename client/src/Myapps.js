14a0861ebaae63c9238f010df587c6077afd4eed
Sing Le <sing.le@accenture.com>
2018-07-06 03:13:43 +1000

    Added more styling changes

--- client/src/Myapps.js
+++ client/src/Myapps.js
@@ -5,20 +5,29 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Grid from '@material-ui/core/Grid';
+import Notifications from '@material-ui/icons/Notifications';
+import Settings from '@material-ui/icons/Settings';
+import VerifiedUser from '@material-ui/icons/VerifiedUser';
+import Person from '@material-ui/icons/Person';
 
 class Myapps extends React.Component{
     render() {
         return(
             <div >
-      <AppBar position="static">
+      <AppBar position="static" style={{backgroundColor: 'rgb(12, 46, 51)'}}>
         <Toolbar>
-          <IconButton  color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit">
-            Title
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Grid container justify={'space-between'}>
+            <Typography variant="title" color="inherit">
+              Team 2 - Carbon Copy
+            </Typography>
+
+            <div>
+              <Notifications style={{marginRight: 20}}/>
+              <Settings style={{marginRight: 20}}/>
+              <Person style={{marginRight: 20}}/>
+            </div>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
-  overrides: {
-    Drawer: { // Name of the component ⚛️ / style sheet
-      dock: { // Name of the rule
-        color: 'white', // Some CSS
-      },
+  palette: {
+    primary: {
+      light: '#002884',
+      main: '#1976d2',
+      dark: '#004ba0',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#88ffff',
+      main: '#4dd0e1',
+      dark: '#009faf',
+      contrastText: '#000',
     },
   },
 });
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
   </MuiThemeProvider>,
   document.getElementById('root'));
