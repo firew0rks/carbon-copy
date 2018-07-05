707471b705cd69fbfa2212495dfe2acec251627e
i.zamir <i.zamir@accenture.com>
2018-07-06 04:49:19 +1000

    Everyone wants a cool install script.

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -1,2 +1,4 @@
+echo "Provisioning elasticsearch mappings and analyzers...."
 curl -XPUT "http://elasticsearch:9200/ticket" -H 'Content-Type: application/json' -d'{  "mappings": {    "ticket": {      "properties": {        "status": {          "type": "keyword"        },        "problem_abstract": {          "type": "text",          "analyzer": "english",          "fields": {            "std": {              "type": "text",              "analyzer": "standard"            }          }        }      }    }  }}'
+echo "Indexing ticket data..."
 elasticsearch_loader --index ticket --es-host "http://elasticsearch:9200" --type ticket json /deepdive.json
