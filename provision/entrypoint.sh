d98801ea80940831758eb25862bff280dd720226
i.zamir <i.zamir@accenture.com>
2018-07-06 04:51:39 +1000

    BORDERS! WHAT ARE WE, ANIMALS?

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -1,4 +1,8 @@
+echo "-----------"
 echo "Provisioning elasticsearch mappings and analyzers...."
+echo "-----------"
 curl -XPUT "http://elasticsearch:9200/ticket" -H 'Content-Type: application/json' -d'{  "mappings": {    "ticket": {      "properties": {        "status": {          "type": "keyword"        },        "problem_abstract": {          "type": "text",          "analyzer": "english",          "fields": {            "std": {              "type": "text",              "analyzer": "standard"            }          }        }      }    }  }}'
+echo "-----------"
 echo "Indexing ticket data..."
+echo "-----------"
 elasticsearch_loader --index ticket --es-host "http://elasticsearch:9200" --type ticket json /deepdive.json
